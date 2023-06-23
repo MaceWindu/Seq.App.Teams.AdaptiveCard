e766fa414b2922672dc34a96f60e9ae8779d75d7
MaceWindu <MaceWindu@users.noreply.github.com>
2023-06-23 15:23:17 +0200

    fix alert template

--- tests/Seq.App.Teams.AdaptiveCard.Tests/TemplateTests.cs
+++ tests/Seq.App.Teams.AdaptiveCard.Tests/TemplateTests.cs
@@ -63,4 +63,148 @@ data",
         Assert.That(errors, Is.Empty);
         Assert.That(result, Is.Not.Null.Or.Empty);
     }
+
+    [Test]
+    public void TestDefaultTemplateNoOptionsData()
+    {
+        var data = new Payload(
+            _event.Id,
+            _event.TimestampUtc.ToString("O"),
+            _event.Data.Level.ToString(),
+            _event.Data.MessageTemplate,
+            _event.Data.RenderedMessage,
+            null,
+            _event.Data.Properties,
+            _event.EventType,
+            "app title",
+            "instance name",
+            "http://localhost");
+
+        using var stream = typeof(TeamsApp).Assembly.GetManifestResourceStream("Seq.App.Teams.AdaptiveCard.Resources.default-template.json")!;
+        using var reader = new StreamReader(stream);
+        var template = reader.ReadToEnd();
+
+        var tmpl = new AdaptiveCardTemplate(template);
+        var result = tmpl.Expand(data);
+        var errors = tmpl.GetLastTemplateExpansionWarnings();
+
+        Assert.That(errors, Is.Empty);
+        Assert.That(result, Is.Not.Null.Or.Empty);
+    }
+
+    [Test]
+    public void TestAlertTemplate()
+    {
+        var props = new Dictionary<string, object?>()
+        {
+            { "NamespacedAlertTitle", "Alert/Name" },
+            { "Alert", new Dictionary<string, object?>()
+            {
+                { "Id", "alert-463"},
+                {"Title", "New Alert" },
+                { "Url", "http://localhost:80/#/alerts/alert-463"},
+                { "OwnerUsername", "admin"},
+                { "SignalExpressionDescription", null},
+                {"Query", "select count(*) as count from stream group by time(1m) having count > 0 limit 100 for background" },
+                { "HavingClause","count > 0"},
+                {"TimeGrouping", "1 minute"}
+            }},
+            {"Source", new Dictionary<string, object?>()
+            {
+                { "RangeStart", "2023-06-22T13:05:43.3585376Z"},
+                { "RangeEnd", "2023-06-22T13:06:43.3585376Z"},
+                {"ResultsUrl", "http://localhost:80/#/events?q=select%20count%28%2A%29%20as%20count%20from%20stream%20group%20by%20time%281m%29%20having%20count%20%3E%200%20limit%20100%20for%20background&from=2023-06-22T13:05:43.3585376Z&to=2023-06-22T13:06:43.3585376Z" },
+                { "Results", new object[]
+                {
+                    new object[]{ "time", "count" },
+                    new object[]{ "2023-06-22T13:05:43.3585376Z", 19 },
+                    new object[]{ "2023-06-22T13:05:44.3585376Z", 5 }
+                } },
+                { "ContributingEvents", new object[]{
+                    new object[]{ "id", "timestamp", "message" },
+                    new object[]{ "event-6d291458732108db9333010000000000", "2023-06-22T13:06:00.5580888Z", "event message 1" },
+                    new object[]{ "event-6d291458732108db9433010000000000", "2023-06-22T13:06:00.5580888Z", "event message 2" }
+                }
+                }
+            }},
+            { "SuppressedUntil", "2023-06-22T13:08:13.3585376Z"},
+            { "Failures", null }
+        };
+        var data = new Payload(
+            _event.Id,
+            _event.TimestampUtc.ToString("O"),
+            _event.Data.Level.ToString(),
+            _event.Data.MessageTemplate,
+            _event.Data.RenderedMessage,
+            _event.Data.Exception,
+            props,
+            2716299265,
+            "app title",
+            "instance name",
+            "http://localhost");
+
+        using var stream = typeof(TeamsApp).Assembly.GetManifestResourceStream("Seq.App.Teams.AdaptiveCard.Resources.default-template.json")!;
+        using var reader = new StreamReader(stream);
+        var template = reader.ReadToEnd();
+
+        var tmpl = new AdaptiveCardTemplate(template);
+        var result = tmpl.Expand(data);
+        var errors = tmpl.GetLastTemplateExpansionWarnings();
+
+        Assert.That(errors, Is.Empty);
+        Assert.That(result, Is.Not.Null.Or.Empty);
+    }
+
+    [Test]
+    public void TestAlertTemplateNoOptionalData()
+    {
+        var props = new Dictionary<string, object?>()
+        {
+            { "NamespacedAlertTitle", "Alert/Name" },
+            { "Alert", new Dictionary<string, object?>()
+            {
+                { "Id", "alert-463"},
+                {"Title", "New Alert" },
+                { "Url", "http://localhost:80/#/alerts/alert-463"},
+                { "OwnerUsername", "admin"},
+                { "SignalExpressionDescription", null},
+                {"Query", "select count(*) as count from stream group by time(1m) having count > 0 limit 100 for background" },
+                { "HavingClause","count > 0"},
+                {"TimeGrouping", "1 minute"}
+            }},
+            {"Source", new Dictionary<string, object?>()
+            {
+                { "RangeStart", "2023-06-22T13:05:43.3585376Z"},
+                { "RangeEnd", "2023-06-22T13:06:43.3585376Z"},
+                {"ResultsUrl", "http://localhost:80/#/events?q=select%20count%28%2A%29%20as%20count%20from%20stream%20group%20by%20time%281m%29%20having%20count%20%3E%200%20limit%20100%20for%20background&from=2023-06-22T13:05:43.3585376Z&to=2023-06-22T13:06:43.3585376Z" },
+                { "Results",  null },
+                { "ContributingEvents",  null }
+            }},
+            { "SuppressedUntil", "2023-06-22T13:08:13.3585376Z"},
+            { "Failures", null }
+        };
+        var data = new Payload(
+            _event.Id,
+            _event.TimestampUtc.ToString("O"),
+            _event.Data.Level.ToString(),
+            _event.Data.MessageTemplate,
+            _event.Data.RenderedMessage,
+            _event.Data.Exception,
+            props,
+            2716299265,
+            "app title",
+            "instance name",
+            "http://localhost");
+
+        using var stream = typeof(TeamsApp).Assembly.GetManifestResourceStream("Seq.App.Teams.AdaptiveCard.Resources.default-template.json")!;
+        using var reader = new StreamReader(stream);
+        var template = reader.ReadToEnd();
+
+        var tmpl = new AdaptiveCardTemplate(template);
+        var result = tmpl.Expand(data);
+        var errors = tmpl.GetLastTemplateExpansionWarnings();
+
+        Assert.That(errors, Is.Empty);
+        Assert.That(result, Is.Not.Null.Or.Empty);
+    }
 }
